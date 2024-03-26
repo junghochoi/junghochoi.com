62871a67e3ba7210f45c57ddb7b0a9a2c0b61c7b
dchoi315 <dchoi315@umd.edu>
2024-03-26 14:46:27 -0400

    photos added, but medium screens not working

--- app/(Home)/_sections/project-section.tsx
+++ app/(Home)/_sections/project-section.tsx
@@ -6,29 +6,19 @@ import Image from "next/image"
 export const Projects = () => {
 	return (
 		<div className="px-5">
-			<h1 className={cn("text-4xl pb-4 text-white", RubikFont.className)}>
-				Projects
-			</h1>
+			<h1 className={cn("text-4xl pb-4 text-white", RubikFont.className)}>Projects</h1>
 
 			<div className="mb-10 bg-slate-800 hover:bg-slate-700 transition-all duration-100  hover:shadow-lg rounded">
 				<div className="flex items-center">
 					<div className="h-full mx-4 p-2">
-						<Image
-							src="/minesweeper.png"
-							width={300}
-							height={300}
-							alt="minesweeper"
-						/>
+						<Image src="/minesweeper.png" width={300} height={300} alt="minesweeper" />
 					</div>
 					<div>
-						<h3 className="text-lg font-semibold text-gray-200">
-							Minesweeper AI Bot
-						</h3>
+						<h3 className="text-lg font-semibold text-gray-200">Minesweeper AI Bot</h3>
 						<div className="mb-4 text-base font-normal text-gray-400">
 							<p>
-								Created Minesweeper online game, and developed an artificial
-								intelligence to provide best moves using logic rules and
-								probability
+								Created Minesweeper online game, and developed an artificial intelligence to provide best moves using
+								logic rules and probability
 							</p>
 						</div>
 					</div>
@@ -38,23 +28,15 @@ export const Projects = () => {
 			<div className="mb-10 bg-slate-800 hover:bg-slate-700 transition-all duration-100  hover:shadow-lg rounded">
 				<div className="flex items-center">
 					<div className="h-full mx-4 p-2">
-						<Image
-							src="/floodfill.png"
-							width={300}
-							height={300}
-							alt="minesweeper"
-						/>
+						<Image src="/floodfill.png" width={300} height={300} alt="minesweeper" />
 					</div>
 
 					<div>
-						<h3 className="text-lg font-semibold text-gray-200">
-							Floodfill AI Bot
-						</h3>
+						<h3 className="text-lg font-semibold text-gray-200">Floodfill AI Bot</h3>
 						<div className="mb-4 text-base font-normal text-gray-400">
 							<p>
-								Created Floodfil online game, and developed an artificial
-								intelligence to provide best moves using logic rules and
-								probability
+								Created Floodfil online game, and developed an artificial intelligence to provide best moves using logic
+								rules and probability
 							</p>
 						</div>
 					</div>
@@ -64,53 +46,19 @@ export const Projects = () => {
 			<div className="mb-10 bg-slate-800 hover:bg-slate-700 transition-all duration-100  hover:shadow-lg rounded">
 				<div className="flex items-center">
 					<div className="h-full mx-4 p-2">
-						<Image
-							src="/yext-hackathon.png"
-							width={300}
-							height={300}
-							alt="minesweeper"
-						/>
+						<Image src="/yext-hackathon.png" width={300} height={300} alt="minesweeper" />
 					</div>
 					<div>
-						<h3 className="text-lg font-semibold text-gray-200">
-							Entity Generator | Yext Hackathon
-						</h3>
+						<h3 className="text-lg font-semibold text-gray-200">Entity Generator | Yext Hackathon</h3>
 						<div className="mb-4 text-base font-normal text-gray-400">
 							<p>
-								Created Minesweeper online game, and developed an artificial
-								intelligence to provide best moves using logic rules and
-								probability
+								Created Minesweeper online game, and developed an artificial intelligence to provide best moves using
+								logic rules and probability
 							</p>
 						</div>
 					</div>
 				</div>
 			</div>
-
-			{/* <div className="mb-10 bg-slate-800 hover:bg-slate-700 transition-all duration-100  hover:shadow-lg rounded">
-				<div className="flex items-center">
-					<div className="h-full mx-4 p-2">
-						<Image
-							className="rounded"
-							src="/floodfill.png"
-							width={300}
-							height={250}
-							alt="yext"
-						/>
-					</div>
-
-					<div>
-						<h3 className="text-lg font-semibold text-gray-200">
-							Entity Generator - Yext Hackathon
-						</h3>
-						<div className="mb-4 text-base font-normal text-gray-400">
-							<p>
-								Use Generative AI to create useful entities for customers when
-								using Yext Platform
-							</p>
-						</div>
-					</div>
-				</div>
-			</div> */}
 		</div>
 	)
 }
--- app/(Home)/_sections/work-section.tsx
+++ app/(Home)/_sections/work-section.tsx
@@ -2,114 +2,107 @@ import { cn } from "@/lib/utils"
 
 import { Badge } from "@/components/ui/badge"
 import { RubikFont } from "@/lib/fonts"
+import Image from "next/image"
+
+const WorkExperience = ({
+	company,
+	title,
+	dateRange,
+	description,
+	imageSrc,
+	skills,
+}: {
+	company: string
+	title: string
+	dateRange: string
+	description: string
+	imageSrc: string
+	skills: string[]
+}) => {
+	return (
+		<li className="mb-10 ms-4 bg-slate-800 hover:bg-slate-700 transition-all duration-100 p-10 hover:shadow-lg rounded">
+			{/* This is the bullet */}
+			<div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
+
+			<div className="flex justify-center ">
+				<div className="w-96 mr-4">
+					<Image
+						src={imageSrc}
+						width={200}
+						height={200}
+						alt="minesweeper"
+						className="rounded  hidden md:block lg:hidden"
+					/>
+				</div>
+
+				<div>
+					<div className="flex flex-row items-center mb-3">
+						<Image
+							src={imageSrc}
+							width={75}
+							height={75}
+							alt="minesweeper"
+							className="rounded mr-4 block md:hidden lg:block"
+						/>
+
+						<div>
+							<h3 className="text-lg font-semibold text-gray-200">
+								{company} <span className="hidden md:inline"> | {title}</span>
+							</h3>
+							<h5 className="text-sm font-medium text-gray-200 md:hidden">{title}</h5>
+							<time className="mb-1 text-sm font-normal leading-none text-gray-200">{dateRange}</time>
+						</div>
+					</div>
+
+					<p className="mb-4 text-base font-normal text-gray-400">{description}</p>
+
+					<div className="flex flex-wrap">
+						{skills.map((skill) => (
+							<Badge className="bg-sky-800 text-gray-300 mr-1.5 mt-2">{skill}</Badge>
+						))}
+					</div>
+				</div>
+			</div>
+		</li>
+	)
+}
 
 export const Work = () => {
 	return (
 		<div className="px-5">
-			<h1 className={cn("text-4xl pb-4 text-white", RubikFont.className)}>
-				Work Experience
-			</h1>
+			<h1 className={cn("text-4xl pb-4 text-white", RubikFont.className)}>Work Experience</h1>
 			<ol className="relative border-s border-gray-200 dark:border-gray-700">
-				<li className="mb-10 ms-4 bg-slate-800 hover:bg-slate-700 transition-all duration-100 p-10 hover:shadow-lg rounded">
-					<div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-					<time className="mb-1 text-sm font-normal leading-none text-gray-200 ">
-						May - Aug 2023
-					</time>
-					<h3 className="text-lg font-semibold text-gray-200">
-						Yext Inc. | Software Engineer Intern
-					</h3>
-					<div className="mb-4 text-base font-normal text-gray-400">
-						<p>
-							Enhanced Yext's backend entity search service by reducing latency
-							on server cold-starts and implementing document search indexing
-						</p>
-					</div>
+				<WorkExperience
+					company="Yext"
+					imageSrc="/yext-hackathon.png"
+					title="Software Engineer Intern"
+					dateRange="May - August 2023"
+					skills={["Java", "Dagger2", "Kafka", "Rabbitmq"]}
+					description="Enhanced Yext's backend entity search service by reducing latency on server cold-starts and
+									implementing document search indexing"
+				/>
 
-					<div className="flex flex-wrap">
-						<Badge className="bg-sky-800 text-gray-300 mr-1.5 mt-2">Java</Badge>
-						<Badge className="bg-sky-800 text-gray-300 mr-1.5 mt-2">
-							Kafka
-						</Badge>
-						<Badge className="bg-sky-800 text-gray-300 mr-1.5 mt-2">
-							Dagger2
-						</Badge>
-						<Badge className="bg-sky-800 text-gray-300 mr-1.5 mt-2">
-							Rabbitmq
-						</Badge>
-					</div>
-				</li>
+				<WorkExperience
+					company="Spring Health"
+					imageSrc="/springhealth.png"
+					title="Software Engineer Intern"
+					dateRange="May - August 2022"
+					skills={["React", "GraphQL", "Ruby On Rails", "PostgreSQL", "Docker", "Typescript"]}
+					description="Designed and Developed Spring Health's in-platform Video Chat Feature, allowing therapists and
+							patients to have sessions without the reliance on third party applications such as Zoom.us or Google
+							Meets"
+				/>
 
-				<li className="mb-10 ms-4 bg-slate-800 hover:bg-slate-700 transition-all duration-100 p-10 hover:shadow-lg rounded">
-					<div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-					<time className="mb-1 text-sm font-normal leading-none text-gray-200 ">
-						May - Aug 2022
-					</time>
-					<h3 className="text-lg font-semibold text-gray-200">
-						Spring Health | Software Engineer Intern
-					</h3>
-					<p className="mb-4 text-base font-normal text-gray-400">
-						Designed and Developed Spring Health's in-platform Video Chat
-						Feature, allowing therapists and patients to have sessions without
-						the reliance on third party applications such as Zoom.us or Google
-						Meets
-					</p>
-					<div className="flex flex-wrap">
-						<Badge className="bg-sky-800 text-gray-300 mr-1.5 mt-2">
-							React
-						</Badge>
-						<Badge className="bg-sky-800 text-gray-300 mr-1.5 mt-2">
-							GraphQL
-						</Badge>
-						<Badge className="bg-sky-800 text-gray-300 mr-1.5 mt-2">
-							Ruby On Rails
-						</Badge>
-						<Badge className="bg-sky-800 text-gray-300 mr-1.5 mt-2">
-							PostgreSQL
-						</Badge>
-						<Badge className="bg-sky-800 text-gray-300 mr-1.5 mt-2">
-							Docker
-						</Badge>
-						<Badge className="bg-sky-800 text-gray-300 mr-1.5 mt-2">
-							Typescript
-						</Badge>
-					</div>
-				</li>
-
-				<li className="mb-10 ms-4 bg-slate-800 hover:bg-slate-700 transition-all duration-100 p-10 hover:shadow-lg rounded">
-					<div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-					<time className="mb-1 text-sm font-normal leading-none text-gray-200 ">
-						June - Aug 2021
-					</time>
-					<h3 className="text-lg font-semibold text-gray-200">
-						Advisar AI. | Software Engineer Intern
-					</h3>
-					<p className="mb-4 text-base font-normal text-gray-400">
-						Designed and Developed Spring Health's in-platform Video Chat
-						Feature, allowing therapists and patients to have sessions without
-						the reliance on third party applications such as Zoom.us or Google
-						Meets
-					</p>
-					<div className="flex flex-wrap">
-						<Badge className="bg-sky-800 text-gray-300 mr-1.5 mt-2">
-							React
-						</Badge>
-						<Badge className="bg-sky-800 text-gray-300 mr-1.5 mt-2">
-							GraphQL
-						</Badge>
-						<Badge className="bg-sky-800 text-gray-300 mr-1.5 mt-2">
-							Redux
-						</Badge>
-						<Badge className="bg-sky-800 text-gray-300 mr-1.5 mt-2">
-							PostgreSQL
-						</Badge>
-						<Badge className="bg-sky-800 text-gray-300 mr-1.5 mt-2">
-							Docker
-						</Badge>
-						<Badge className="bg-sky-800 text-gray-300 mr-1.5 mt-2">
-							Typescript
-						</Badge>
-					</div>
-				</li>
+				<WorkExperience
+					company="Advisar AI"
+					imageSrc="/advisarai.jpeg"
+					title="Software Engineer Intern"
+					dateRange="May - August 2021"
+					skills={["React", "GraphQL", "Ruby On Rails", "PostgreSQL", "Docker", "Typescript"]}
+					description="Designed and Developed Spring Health's in-platform Video Chat Feature, allowing therapists and
+							patients to have sessionsfsfsthoudfsdfasdfa sdfas dfasdffsdfajklsdfjkl;asdjkl;asdfjkl;fasjkl;asdft the reliance on third party applications such as Zoom.us or Google
+							Meets"
+				/>
 			</ol>
 		</div>
 	)
